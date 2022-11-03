cbaef2548b7d66dd69304620c8df23337de78164
a47527 <a47527@alunos.ipb.pt>
2022-11-03 15:34:24 +0000

    1. Time - a47527

--- Assets/Script/SecondsUpdate.cs
+++ Assets/Script/SecondsUpdate.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class SecondsUpdate : MonoBehaviour
 {
+    float timeStartOffset = 0;
+    bool getStartTime = false;
+    
     void Update()
     {
+        if(!getStartTime)
+        {
+            timeStartOffset = Time.realtimeSinceStartup;
+            getStartTime = true;
+        }
+
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, Time.realtimeSinceStartup - timeStartOffset);
+
 
     }
 }
